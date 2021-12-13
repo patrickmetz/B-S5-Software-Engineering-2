80c4b32c0bec3d05e54dcd7412e11c76de0597d4
patrickmetz <patrickmetz@web.de>
2021-12-13 15:54:40 +0100

    fixed spelling

--- src/main/java/org/hbrs/se2/project/hellocar/control/factories/AbstractJobPortalUserFactory.java
+++ src/main/java/org/hbrs/se2/project/hellocar/control/factories/AbstractJobPortalUserFactory.java
@@ -3,7 +3,7 @@ package org.hbrs.se2.project.hellocar.control.factories;
 import org.hbrs.se2.project.hellocar.dtos.account.JobPortalUserDTO;
 import org.hbrs.se2.project.hellocar.entities.JobPortalUser;
 
-public abstract class AbstractJobPortalUserFactory extends AbstractlUserFactory{
+public abstract class AbstractJobPortalUserFactory extends AbstractUserFactory {
     public abstract JobPortalUser create();
 
     public void setupEntityByDto(JobPortalUser entity, JobPortalUserDTO dto) {
